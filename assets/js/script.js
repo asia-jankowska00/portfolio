e782682b7a9512000c7b8baaa3741d7a3b4a915a
Joanna Jankowska <asia.jankowska00@gmail.com>
2020-04-28 12:31:39 +0200

    All pages added, responsiveness, gradient bg

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,9 @@
 
 const menuHamburger = document.getElementById("menuHamburger");
 const menuUl = document.getElementById("menuUl");
+const header = document.getElementsByTagName("header")[0];
 
 menuHamburger.addEventListener("click", () => {
   menuUl.classList.toggle("display-none");
+  header.classList.toggle("overflow-visible");
 });
