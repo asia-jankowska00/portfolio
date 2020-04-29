4bceb3aea5e0bef8fe186d06380a658f90a31db7
Joanna Jankowska <asia.jankowska00@gmail.com>
2020-04-29 12:37:10 +0200

    Cases, optimizations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,52 @@
-// const mobileMenu = document.getElementById('mobile-menu');
-// const hamburger = document.getElementById('hamburger');
-// const cross = document.getElementById('cross');
-// const mobileNavMenus = document.getElementById('mobile-nav-menus');
-// const mobileNavMenusUl = document.getElementById('mobile-nav-menus-ul');
+const $ = (arg) => {
+  return document.querySelector(arg);
+};
 
-// hamburger.addEventListener('click', function () {
-//     mobileMenu.classList.toggle('mobile-menu-hidden');
-//     this.classList.toggle('hidden');
-//     cross.classList.toggle('hidden');
-// })
+const $$ = (arg) => {
+  return document.querySelectorAll(arg);
+};
 
-// cross.addEventListener('click', function () {
-//     mobileMenu.classList.toggle('mobile-menu-hidden');
-//     this.classList.toggle('hidden');
-//     hamburger.classList.toggle('hidden');
-// })
-
-// mobileNavMenus.addEventListener('click', function () {
-//     mobileMenu.classList.toggle('mobile-menu-expanded')
-//     mobileNavMenus.classList.toggle('menus-li-expanded')
-// })
-
-const menuHamburger = document.getElementById("menuHamburger");
-const menuUl = document.getElementById("menuUl");
-const header = document.getElementsByTagName("header")[0];
+const menuHamburger = $("#menuHamburger");
+const menuUl = $("#menuUl");
+const header = $("header");
+const navDesktop = $(".nav-desktop");
+const navDesktopLis = navDesktop.querySelectorAll("li");
+const body = $("body");
 
 menuHamburger.addEventListener("click", () => {
   menuUl.classList.toggle("display-none");
   header.classList.toggle("overflow-visible");
 });
+
+const checkNavPosition = () => {
+  if (window.location.pathname === "/index.html") {
+    scrollNavStyler(0.1);
+  } else {
+    scrollNavStyler(0.4);
+  }
+};
+
+const scrollNavStyler = (offsetPercent) => {
+  let headerHeight = header.offsetHeight;
+  headerHeight -= headerHeight * offsetPercent;
+  if (window.scrollY > headerHeight) {
+    navDesktop.classList.add("invert");
+    navDesktopLis.forEach((li) => {
+      li.classList.remove("white-underline-hover");
+      li.classList.add("gradient-underline-hover");
+    });
+  } else {
+    navDesktop.classList.remove("invert");
+    navDesktopLis.forEach((li) => {
+      li.classList.remove("gradient-underline-hover");
+      li.classList.add("white-underline-hover");
+    });
+  }
+  if (window.scrollY > headerHeight * 1.15) {
+    navDesktop.classList.add("width-100");
+  } else {
+    navDesktop.classList.remove("width-100");
+  }
+};
+
+window.addEventListener("scroll", checkNavPosition);
