4b068b4e79227dabdb994cb1c76b15b7e5289234
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-09-15 11:28:08 +0200

    Add new intro section

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,9 +22,9 @@ const Homepage = () => {
       <Layout isHomepage={true} title="Home">
         <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
           <section className="padding-x-medium mt-25 padding-y-large blurb show-on-scroll">
-            <h1 className="text-center">&lt;Front end developer /&gt;</h1>
-            <h4 className="text-center subtitle">
-              "A person who creates user interfaces and websites."
+            <h1 className="text-center">&lt;The developer you need /&gt;</h1>
+            <h4 className="text-center subtitle ">
+              "Always leave a place better than you found it."
             </h4>
             <div className="flex">
               <img
@@ -33,14 +33,63 @@ const Homepage = () => {
                 alt=""
               />
               <div className="flex blurb-content">
-                <p className="padding-y-small">
-                  That's me! But I do more than just build websites and
-                  interfaces –
-                  <br />I create elegant, thoughtful
-                  <strong> digital experiences</strong>,<br />
-                  that <strong>delight</strong> the users and
-                  <strong> satisfy</strong> stakeholders' expectations.
+                <p className="padding-y-extra-small">
+                  Technology is making the world a better place. I strive to
+                  make my contribution to these improvements by{" "}
+                  <strong>creating digital solutions</strong> that will help
+                  people on a daily basis.
                 </p>
+                <p className="mb-4">
+                  As a developer, I’m <strong>curiously inventive</strong>,{" "}
+                  <strong>relentlessly devoted</strong>, and most importantly,{" "}
+                  <strong>addicted to learning</strong>. Every day, I try to
+                  become a little better at what I do than I was the day before.
+                </p>
+                <h4 className="subtitle">
+                  So, what can I do, you ask? Here are some examples:
+                </h4>
+                <ul className="font-secondary">
+                  <li>
+                    <p>
+                      Overhaul your <strong>landing page</strong>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Develop a <strong>static website</strong>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Create a <strong>React</strong> component library
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Translate design into <strong>code</strong>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Set up a <strong>full-stack</strong> platform or
+                      application
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Integrate a <strong>database</strong> or an{" "}
+                      <strong>API</strong>
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      Come up with <strong>design</strong> solutions
+                    </p>
+                  </li>
+                  <li>
+                    <p>…and more</p>
+                  </li>
+                </ul>
                 <div className="flex blurb-links">
                   <Link to="/skills">
                     <h4 className="gradient-underline-hover padding-y-extra-small">
