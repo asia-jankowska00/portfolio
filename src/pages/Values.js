4fc40d40063c4ce556c75ce86096134c0c559af2
asia-jankowska00 <asia.jankowska00@gmail.com>
2020-09-23 15:56:45 +0200

    Add values, change main font

--- src/pages/Values.js
+++ src/pages/Values.js
@@ -8,6 +8,45 @@ const Values = () => {
   return (
     <Layout title="Values" textToType="< Values />">
       <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+        <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+          <section className="padding-x-large padding-y-small flex values show-on-scroll">
+            <div className="values-icons">
+              <h1>Ambition</h1>
+              <h4 className="subtitle">"Always one step ahead"</h4>
+            </div>
+            <div className="values-text">
+              <p>
+                With each new project I take up new technologies, tools and
+                methods. I like to be ahead of the game and keep up to date with
+                trends and tech.
+              </p>
+              <p>
+                My expertise allows me to to eagerly anticipate challenges
+                instead of fearing them.
+              </p>
+            </div>
+          </section>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
+          <section className="padding-x-large padding-y-small flex values show-on-scroll">
+            <div className="values-text">
+              <p>
+                Half-measures and doing the minimum won't do for me. If I take
+                up a task, I will do it well.
+              </p>
+              <p>
+                I consider all aspects of a project and make sure that none of
+                them will fall short as a result of my decisions.
+              </p>
+            </div>
+            <div className="values-icons">
+              <h1>Excellence</h1>
+              <h4 className="subtitle">"Never the bare minimum"</h4>
+            </div>
+          </section>
+        </ScrollAnimation>
+
         <section className="padding-x-large padding-y-small flex values show-on-scroll">
           <div className="values-icons">
             <h1>Creativity</h1>
@@ -30,56 +69,17 @@ const Values = () => {
         <section className="padding-x-large padding-y-small flex values show-on-scroll">
           <div className="values-text">
             <p>
-              With each new project I take up new technologies, tools and
-              methods. I like to be ahead of the game and keep up to date with
-              trends and tools.
+              A well-integrated team is the first step to achieving success.
             </p>
             <p>
-              My expertise allows me to to eagerly anticipate challenges instead
-              of fearing them.
+              Honesty and efficient communication are always a priority for me,
+              and I am ready to set aside my personal preferences if it means
+              achieving results faster.
             </p>
           </div>
           <div className="values-icons">
-            <h1>Ambition</h1>
-            <h4 className="subtitle">"Always one step ahead"</h4>
-          </div>
-        </section>
-      </ScrollAnimation>
-      {/*  */}
-      <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
-        <section className="padding-x-large padding-y-small flex values show-on-scroll">
-          <div className="values-icons">
-            <h1>Empathy</h1>
-            <h4 className="subtitle">"What I make, I make for you"</h4>
-          </div>
-          <div className="values-text">
-            <p>
-              I always consider who the creation is aimed at, and how it can
-              best serve it's purpose.
-            </p>
-            <p>
-              I supply my decisions with ample research and established best
-              practices.
-            </p>
-          </div>
-        </section>
-      </ScrollAnimation>
-
-      <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
-        <section className="padding-x-large padding-y-small flex values show-on-scroll">
-          <div className="values-text">
-            <p>
-              Half-measures and doing the minimum won't do for me. If I take up
-              a task, I will do it well.
-            </p>
-            <p>
-              I consider all aspects of a project and make sure that none of
-              them will fall short as a result of my decisions.
-            </p>
-          </div>
-          <div className="values-icons">
-            <h1>Excellence</h1>
-            <h4 className="subtitle">"Never the bare minimum"</h4>
+            <h1>Teamwork</h1>
+            <h4 className="subtitle">"Great things are achieved together"</h4>
           </div>
         </section>
       </ScrollAnimation>
@@ -87,15 +87,20 @@ const Values = () => {
       <ScrollAnimation animateIn="fadeIn" animateOnce={true}>
         <section className="padding-x-large padding-y-small flex values show-on-scroll">
           <div className="values-icons">
-            <h1>Focus</h1>
-            <h4 className="subtitle">“And razor sharp precision”</h4>
+            <h1>Honesty</h1>
+            <h4 className="subtitle">“Because you can’t grow without it”</h4>
           </div>
           <div className="values-text">
             <p>
-              I set precise tasks and goals and aim to achieve them. I always
-              look for ways to work more efficiently.
+              I appreciate honest feedback, and I’m ready to give it when the
+              situation calls for it. I like getting straight to the point and
+              not wasting time on unnecessary courtesy.
             </p>
-            <p></p>
+            <p>
+              I am always honest and open with my perception of the situation,
+              and I will make it clear when I don’t understand something or
+              think I’m not capable of doing it.
+            </p>
           </div>
         </section>
       </ScrollAnimation>
