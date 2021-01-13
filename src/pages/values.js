252e0f1a2f5e5619bcd2f174e3e013c519ca2e57
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-01-13 18:52:03 +0100

    update values and profile picture

--- src/pages/values.js
+++ src/pages/values.js
@@ -16,12 +16,13 @@ const Values = () => {
             </div>
             <div className="values-text">
               <p>
-                With each new project I take up new technologies, tools and
-                methods. I like to be ahead of the game and keep up to date with
-                trends and tech.
+                With each new project I take up{" "}
+                <strong>new technologies</strong>, tools and methods. I like to
+                be ahead of the game and keep up to date with trends and tech.
               </p>
               <p>
-                My expertise allows me to to eagerly anticipate challenges
+                My expertise allows me to to eagerly anticipate{" "}
+                <strong>challenges</strong>{" "}
                 instead of fearing them.
               </p>
             </div>
@@ -33,11 +34,12 @@ const Values = () => {
             <div className="values-text">
               <p>
                 Half-measures and doing the minimum won't do for me. If I take
-                up a task, I will do it well.
+                up a task, I will do it <strong>well</strong>.
               </p>
               <p>
-                I consider all aspects of a project and make sure that none of
-                them will fall short as a result of my decisions.
+                I consider <strong>all aspects</strong> of a project and make
+                sure that none of them will fall short as a result of my
+                decisions.
               </p>
             </div>
             <div className="values-icons">
@@ -54,12 +56,13 @@ const Values = () => {
           </div>
           <div className="values-text">
             <p>
-              The act of creation is my greatest passion, and I make sure that
-              it shows in my efforts.
+              The art of <strong>creation</strong> is my greatest passion, and I
+              make sure that it shows in my efforts.
             </p>
             <p>
               I love shaping things from the ground up, and I’m always bursting
-              with ideas on how to make the best from what I’m given.
+              with <strong>ideas</strong> on how to make the best from what I’m
+              given.
             </p>
           </div>
         </section>
@@ -69,12 +72,14 @@ const Values = () => {
         <section className="padding-x-large padding-y-small flex values show-on-scroll">
           <div className="values-text">
             <p>
-              A well-integrated team is the first step to achieving success.
+              A well-integrated <strong>team</strong> is the first step to
+              achieving success.
             </p>
             <p>
-              Honesty and efficient communication are always a priority for me,
-              and I am ready to set aside my personal preferences if it means
-              achieving results faster.
+              <stron>Honesty</stron> and{" "}
+              <strong>efficient communication</strong> are always a priority for
+              me, and I am ready to set aside my personal preferences if it
+              means achieving results faster.
             </p>
           </div>
           <div className="values-icons">
@@ -92,14 +97,14 @@ const Values = () => {
           </div>
           <div className="values-text">
             <p>
-              I appreciate honest feedback, and I’m ready to give it when the
-              situation calls for it. I like getting straight to the point and
-              not wasting time on unnecessary courtesy.
+              I appreciate <strong>honest feedback</strong>, and I’m ready to
+              give it when the situation calls for it. I like getting straight
+              to the point and not wasting time on unnecessary courtesy.
             </p>
             <p>
-              I am always honest and open with my perception of the situation,
-              and I will make it clear when I don’t understand something or
-              think I’m not capable of doing it.
+              I am always <strong>honest</strong> and <strong>open</strong> with my perception of the situation,
+              and I will <strong>make it clear</strong> when I don’t understand
+              something or think I’m not capable of doing it.
             </p>
           </div>
         </section>
