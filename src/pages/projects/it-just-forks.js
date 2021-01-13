5a12de6f4aa239e99e74cc32de930828d2de084d
asia-jankowska00 <asia.jankowska00@gmail.com>
2021-01-13 18:50:54 +0100

    remove it-just-forks

--- src/pages/projects/it-just-forks.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import ProjectPage from "../../components/ProjectPage"
-
-const ItJustForksProject = props => {
-  return <ProjectPage projectPath="it-just-forks"></ProjectPage>
-}
-
-export default ItJustForksProject
